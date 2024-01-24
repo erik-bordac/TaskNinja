f54c323ab26eded8af31ff24a7ea0de086144c7a
bordace <erik.bordac@gmail.com>
2024-01-24 20:03:20 +0100

    fix GetTeamsById query bug

--- TaskNinja/Services/TeamsService.cs
+++ TaskNinja/Services/TeamsService.cs
@@ -14,8 +14,9 @@ namespace TaskNinja.Services
         }
         public async Task<IEnumerable<Team>> GetTeamsByMember(string userId)
         {
-            var l = await _db.Team.ToListAsync();
-            return l.Where(x => x.Members.All(m => m.Id == userId));
+            //var l = await _db.Team.ToListAsync();
+            return _db.Team.Include(x => x.Members).Where(x => x.Members.Select(m => m.Id).Contains(userId));
+            //return l.Where(x => x.Members.All(m => m.Id == userId));
         }
     }
 }
